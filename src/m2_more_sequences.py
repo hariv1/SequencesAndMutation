4872a13176cb42ea794b6acc9a06d4483e3ad9a8
hariv1 <hariv@rose-hulman.edu>
2018-01-17 16:01:34 -0500

    DONE

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -10,8 +10,8 @@ for ITERATING through SEQUENCES, including selections from:
   -- Looking at two sequences in parallel
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Vikram Hari.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -103,9 +103,17 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    shortest_string_index = 0
+
+    for k in range(len(strings)-1):
+        if len(strings[k+1]) < len(strings[shortest_string_index]):
+            shortest_string_index = k+1
+
+    return strings[shortest_string_index]
 
 
 def run_test_index_of_largest_number():
@@ -180,9 +188,17 @@ def index_of_largest_number(numbers, n):
 
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    max_index = 0
+
+    for k in range(n-1):
+        if numbers[k+1] > numbers[max_index]:
+            max_index = k+1
+
+    return max_index
 
 
 # ----------------------------------------------------------------------
@@ -235,9 +251,17 @@ def number_of_stutters(s):
        :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    count = 0
+
+    for k in range(len(s)-1):
+        if s[k+1] == s[k]:
+            count = count + 1
+
+    return count
 
 
 def run_test_is_palindrome():
