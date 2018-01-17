22ef56c6935c4e4e996a009ab056779204f12473
hariv1 <hariv@rose-hulman.edu>
2018-01-17 16:36:30 -0500

    DONE

--- src/m6_mutation.py
+++ src/m6_mutation.py
@@ -4,8 +4,8 @@ In this module, you mutate by CHANGING elements of a list.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Vikram Hari.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # ----------------------------------------------------------------------
 # STUDENTS:
@@ -129,10 +129,22 @@ def RETURN_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # DONE: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
+
+
+    new_list = []
+
+    for k in range(len(numbers)):
+        if numbers[k] > 0:
+            new_list.append(numbers[k])
+
+        else:
+            new_list.append(0)
+
+    return new_list
 
 
 def run_test_MUTATE_replace_negatives_by_zeros():
@@ -185,7 +197,7 @@ def MUTATE_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # DONE: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
@@ -194,5 +206,15 @@ def MUTATE_replace_negatives_by_zeros(numbers):
 # If this module is running at the top level (as opposed to being
 # imported by another module), then call the 'main' function.
 # ----------------------------------------------------------------------
+
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            numbers[k] = 0
+
+
+
+
+
+
 if __name__ == '__main__':
     main()
